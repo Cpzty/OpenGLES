8e3b3930a0eeb0e5f18bca2c76884542f0d4fefe
Cpzty <Cristianpzty@gmail.com>
2018-03-28 04:51:04 -0600

    Cuadrado

--- app/src/main/java/com/example/cristian/opengles/Stage.java
+++ app/src/main/java/com/example/cristian/opengles/Stage.java
@@ -1,10 +1,15 @@
 package com.example.cristian.opengles;
 
 import android.content.Context;
+import android.opengl.GLES10;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -19,11 +24,27 @@ public class Stage extends GLSurfaceView {
     private float w,h;
     private int screenW,screenH;
 
+    private FloatBuffer vertexBuffer;
+
     @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     public Stage(Context context, AttributeSet attrs) {
         super(context, attrs);
         setEGLConfigChooser(8,8,8,8,0,0);
         setRenderer(new MyRenderer());
+
+        float vertices[] = {
+                -0.5f,-0.5f,0.0f,
+                0.5f,-0.5f,0.0f,
+                -0.5f,0.5f,0.0f,
+                0.5f,0.5f,0.0f
+
+        };
+
+        ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+        vbb.order(ByteOrder.nativeOrder());
+        vertexBuffer = vbb.asFloatBuffer();
+        vertexBuffer.put(vertices);
+        vertexBuffer.position(0);
     }
 
     private final class MyRenderer implements GLSurfaceView.Renderer{
@@ -74,6 +95,14 @@ public class Stage extends GLSurfaceView {
 
         @Override
         public void onDrawFrame(GL10 gl10) {
+
+            gl10.glPushMatrix();
+            gl10.glClear(GLES10.GL_COLOR_BUFFER_BIT);
+            gl10.glTranslatef(w/2,h/2,0);
+            gl10.glScalef(120,100,0);
+            gl10.glVertexPointer(3,GL10.GL_FLOAT,0,vertexBuffer);
+            gl10.glDrawArrays(GL10.GL_TRIANGLE_STRIP,0,4);
+            gl10.glPopMatrix();
 
         }
     }
